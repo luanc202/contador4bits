42205c810db15bee5a5cc51631e4b33fe6335758
Luan <45375400+luancosta202@users.noreply.github.com>
2019-06-25 19:40:40 -0300

    Removidos comentários

--- contavvvr.vhd
+++ contavvvr.vhd
@@ -3,15 +3,6 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use ieee.std_logic_unsigned.all;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
-
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity counter is
     Port ( clk : in STD_LOGIC;
        rst: in STD_LOGIC;
